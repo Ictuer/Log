1c93e232c1ca6ae093db881847511a2474ffff4a
ictuer <ictuer@gmail.com>
2021-06-29 00:04:35 +0700

    logs detail

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var dotenv = require('dotenv')
 var mongoose = require('mongoose')
 var JWT = require('./jwt')
+var { formatDistance } = require('date-fns')
 
 var app = express()
 var bodyParser = require('body-parser')
@@ -15,8 +16,19 @@ app.get('/', (req, res) => {
     res.json({ success: true })
 })
 
+//{type: 'bet', tool: 'baccarat', mac: '14:7d:da:c6:19:9a'}
 app.get('/logs', (req, res) => {
-   Log.find().then(data => res.json(data.map(i => i.type)))
+    Log.find().then(data => res.json(data.map(i => {
+        switch(i.type) {
+            case 'bet':
+                return `Bet ${i.data.total_bet} for ${i.data.for} => ${i.data.result} |${i.data.total} |${formatDistance(i.timestamps, Date.now())}`
+            case 'login':
+                return `Login | Total: ${i.data.total } | ${formatDistance(i.timestamps, Date.now())}`
+            case 'limit':
+                return `${i.data.type.toUpperCase()} ${i.data.type == 'profit_limit' ? i.data.profit_limit : i.data.loss_limit } | ${formatDistance(i.timestamps, Date.now())}`
+        }
+        return i.type + " " + formatDistance(i.timestamps, Date.now())
+    })))
 })
 
 app.post('/log', (req, res) => {
